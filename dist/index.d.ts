49c0e5215f87e4317b39b07e403e176d6384b295
Agustin Mendez <agustin.mz.92@gmail.com>
2019-12-16 02:16:27 -0300

    remove dist

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-export declare function keccakHash(): Promise<{
-    update: (data: string | number[] | Uint8Array) => void;
-    digest: {
-        (): string;
-        (hexResult: false): Uint8Array;
-        (hexResult: true): string;
-    };
-}>;
-export default keccakHash;
